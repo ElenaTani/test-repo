5da49ee607bf9376db91948a0889dc31db29fe2b
Olena Hrechana <hrechanaod@gmail.com>
2024-04-15 19:55:17 +0200

    my first homework

--- myfile.js
+++ myfile.js
@@ -1,3 +1,16 @@
-// my homework2
-// my homework3
-console.log('hello world');
+'use strict';
+
+let x : Number = 10;
+let y = 2;
+let sum = x+y;
+let diff = x-y;
+let product = x * y;
+let quotient = x / y;
+let expon = y ** x;
+console.log(x);
+console.log(y);
+console.log(sum);
+console.log(diff);
+console.log(product);
+console.log(quotient);
+console.log(expon);
