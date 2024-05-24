3e7b72cbe56ffc48d4783e6b6e5d3aa0899c5260
Olena Hrechana <hrechanaod@gmail.com>
2024-05-24 19:50:27 +0200

    homework8

---
+++ homeworks/hw8/hw8-2.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function someFunction(...names) {
+    console.log(`Greetings ${names.join(', ')}`)
+}
+
+function slower(func, seconds) {
+    return function (...names) {
+        console.log(`Chill out, you will get you result in ${seconds} seconds`);
+        setTimeout(func, seconds*1000, ...names)
+    }
+}
+
+let slowedSomeFunction = slower(someFunction, 5);
+
+slowedSomeFunction('Anna', 'Petro', 'Robert')
