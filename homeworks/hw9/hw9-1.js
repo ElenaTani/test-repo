096c84e43fe9f9d540e0f1114b48a0316371364c
Olena Hrechana <hrechanaod@gmail.com>
2024-05-26 15:48:01 +0200

    homework9

---
+++ homeworks/hw9/hw9-1.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let element = document.querySelector('#headerTwo');
+console.log(element);
+
+console.log(document.querySelector('.firstSection'));
+
+let listItem = document.getElementsByTagName('ul')[0].lastElementChild.previousElementSibling;
+console.log(listItem);
+
+let hatredLevelBlockClassElement = document.getElementsByClassName('hatredLevelBlock')[0];
+console.log(hatredLevelBlockClassElement);          
